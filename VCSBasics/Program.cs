4448834c33a3c90c92fde1c3ff45e44972314a80
DESKTOP-MQP0SAA\Pravat <mujahmaskey@gmail.com>
2020-12-05 21:54:24 -0500

    added doc

--- VCSBasics/Program.cs
+++ VCSBasics/Program.cs
@@ -4,9 +4,13 @@ namespace VCSBasics
 {
     class Program
     {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Git!");
         }
     }
 }
